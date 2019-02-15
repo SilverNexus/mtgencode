d0fa2708874b81684ec14c22552b0bde92bab9e8
SilverNexus <a_daniel.hawkins@yahoo.com>
2019-02-15 12:00:11 -0500

    Exclude the Holiday cards, as they are essentially un-set-like anyway.

--- lib/jdecode.py
+++ lib/jdecode.py
@@ -68,7 +68,7 @@ def mtg_open_json(fname, verbose = False):
 
 # filters to ignore some undesirable cards, only used when opening json
 def default_exclude_sets(cardset):
-    return cardset == 'Unglued' or cardset == 'Unhinged' or cardset == 'Celebration' or cardset == 'Unstable'
+    return cardset in ['Unglued', 'Unhinged', 'Celebration', 'Unstable', 'Happy Holidays']
 
 def default_exclude_types(cardtype):
     return cardtype in ['conspiracy']
